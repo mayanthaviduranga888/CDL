9642bdcca704daef8a23f39127f6deec73444a9d
mayanthaviduranga888 <mayantha.viduranga888@gmail.com>
2026-05-30 17:19:53 +0530

    Add dist build artifacts and update .gitignore
    Add generated production build output under dist (client assets, server bundles, SSR runtime, images and config files) and a wrangler deploy config pointing to dist/server/wrangler.json. Also update .gitignore to ignore .vercel and environment files (.env*). These changes include build artifacts and a deployment config to enable deployment from the built output.

---
+++ dist/server/_tanstack-start-manifest_v-D2alCiGC.mjs
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/mayanthaviduranga/Documents/Ceylon Digital Labs (PVT) Ltd./CDL 2.0/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-BrOKR8BS.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-BrOKR8BS.js" } }] }, "/": { filePath: "/Users/mayanthaviduranga/Documents/Ceylon Digital Labs (PVT) Ltd./CDL 2.0/src/routes/index.tsx", children: void 0, css: ["/assets/index-Ba_UcWrB.css"], preloads: ["/assets/index-D5NlovwV.js"] } } });
+export {
+  tsrStartManifest
+};
